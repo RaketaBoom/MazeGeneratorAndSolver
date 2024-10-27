1f0b82fc692cd9de380ff883a8a97c654a541d6f
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-10-27 20:11:07 +0400

    test: add test for rendering path and working of A* algorithm

--- src/test/java/backend/academy/maze/solvers/impl/AStarAlgorithmTest.java
+++ src/test/java/backend/academy/maze/solvers/impl/AStarAlgorithmTest.java
@@ -5,9 +5,9 @@ import backend.academy.maze.models.Coordinate;
 import backend.academy.maze.models.GraphMaze;
 import backend.academy.maze.models.Solution;
 import backend.academy.maze.solvers.Solver;
-import org.junit.jupiter.api.Test;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AStarAlgorithmTest {
     Solver solver = new AStarAlgorithm();
